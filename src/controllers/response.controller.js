a6953485c91bf322d032eb590a19cf6592ba291d
Nguyễn Tiến Thành <nguyentienthanh.tgdd@gmail.com>
2024-03-19 01:27:37 +0700

    Fix delete

--- src/controllers/response.controller.js
+++ src/controllers/response.controller.js
@@ -6,15 +6,15 @@ export const create = catchAsync(async (req, res, next) => {
   const { content, survey_id, field_id, response_id } = req.body;
 
   if (!content || !survey_id || !field_id || !response_id)
-    return res
-      .status(400)
-      .json({ message: `Content, survey id, field id, response id can not be empty` });
+    return res.status(400).json({
+      message: `Content, survey id, field id, response id can not be empty`
+    });
 
   var field = new Response({
     content,
     survey_id,
     field_id,
-    response_id, 
+    response_id,
     owner_id: req.user.user._id,
     createdAt: new Date(),
     updatedAt: new Date()
@@ -40,19 +40,44 @@ export const deleteById = catchAsync(async (req, res, next) => {
 
   if (!_id) throw new Error(`Response ID not provided`);
 
-  const response = await Response.findById(_id);
+  const response = await Response.find({ response_id: _id });
   if (!response) return res.status(404).json({ error: 'Response not found' });
 
-  baseAirtable('responses').destroy(_id, async (err) => {
-    if (err) {
-      console.error('Error deleting fields from Airtable:', err);
-      throw new Error('Internal server error');
-    }
+  baseAirtable('responses')
+    .select({
+      filterByFormula: `{response_id} = "${_id}"`
+    })
+    .eachPage((records) => {
+      const responseIds = records.map((record) => record.id);
 
-    await Response.deleteOne({ _id });
+      if (responseIds.length > 0) {
+        baseAirtable('responses').destroy(responseIds, async (err) => {
+          if (err) {
+            console.error('Error deleting fields from Airtable:', err);
+            return reject(err);
+          }
+          await Response.deleteMany({ response_id: _id });
 
-    return res
-      .status(200)
-      .json({ success: true, message: 'Deleted successfully' });
-  });
+          return res
+            .status(200)
+            .json({ success: true, message: 'Deleted successfully' });
+        });
+      } else {
+        return res
+          .status(204)
+          .json({ success: true, message: 'No response id founded' });
+      }
+    });
+  // baseAirtable('responses').destroy(_id, async (err) => {
+  //   if (err) {
+  //     console.error('Error deleting fields from Airtable:', err);
+  //     throw new Error('Internal server error');
+  //   }
+
+  //   await Response.deleteOne({ _id });
+
+  //   return res
+  //     .status(200)
+  //     .json({ success: true, message: 'Deleted successfully' });
+  // });
 });
